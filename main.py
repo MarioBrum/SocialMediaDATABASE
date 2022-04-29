c5f49616bdbce674d99beeb0a2376662652371c8
MarioBrum <mario.augusto@acad.pucrs.br>
2022-04-29 11:25:20 -0300

    horarios formatados
    precisa fazer menu, fazer pesquisas genericas a partir dos horarios(horario com maior engajamento/impressoes...)
    fazer pesquisas por palavras nos tweets com retorno de engajamento/impressoes etc..

--- main.py
+++ main.py
@@ -84,25 +84,45 @@ def maiorGenerico(listaTweets,termo):
             maiorTweet = l
     return maiorTweet.funcRetorno()
     
-'''
+''' lembrar de usar isso no menu
 maiorGenerico(listaTweetsCompletos,"engajamentos")
 maiorGenerico(listaTweetsCompletos,"impressoes")
 maiorGenerico(listaTweetsCompletos,"retweets")
 maiorGenerico(listaTweetsCompletos,"favoritos")
 '''
 ##vou ter que fazer média
-def horariosComMaisPostagens(listaTweets):
+def formataHorarios(listaTweets):
     listaDeHorarios = []
+    listaDeTweetsOrdenadaPorHorario = []
+    for i in range (0,24):
+        listaDeTweetsOrdenadaPorHorario.append([])
     for i in range (0,24):
         listaDeHorarios.append(0)
+    #print(listaDeTweetsOrdenadaPorHorario) #so pra confirmar que foi criado uma lista de lista com os tweets ordenados por horario
     for l in listaTweets:
         vetorHorarios = l.horario.split()       #separa apenas o horario
         #print(vetorHorarios[1])
         #print(int(vetorHorarios[1][0:2]))       #numero apenas da HORA do tweet
         listaDeHorarios[int(vetorHorarios[1][0:2])] = listaDeHorarios[int(vetorHorarios[1][0:2])] + 1
-    print(listaDeHorarios)
+        listaDeTweetsOrdenadaPorHorario[int(vetorHorarios[1][0:2])].append(l)
 
-    #print(listaDeHorarios)
-    #print(len(listaDeHorarios))
+    return(listaDeHorarios,listaDeTweetsOrdenadaPorHorario) ## retorna uma lista com os numeros de tweets
+                                                            ## e com os tweets onde em ambas listas cada indice
+                                                            ## é uma hora de 0-24
 
-horariosComMaisPostagens(listaTweetsCompletos)
+estruturaDeDadosHorarios = formataHorarios(listaTweetsCompletos) ##cria as duas estruturas para utilizar horarios
+
+#retorna o numero 3 dados
+#horario onde mais posta/numero de Tweets do mesmo/total de tweets
+def horarioComMaisTweets(listaDeHorarios):
+    maior = listaDeHorarios[0]
+    indiceHora = 0
+    total = 0
+    for i in range(0,len(listaDeHorarios)):
+        total += i
+        if listaDeHorarios[i] > maior:
+            maior = listaDeHorarios[i]
+            indiceHora = i   
+    return(indiceHora,maior,total)  #retorna o horario/numero de Tweets do mesmo/total de tweets
+
+#print(horarioComMaisTweets(estruturaDeDadosHorarios[0]))  #horario que mais postou, quantas postagens
